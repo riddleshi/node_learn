702087c1863721b30ce68e4fbd28f0e4f9286ee7
riddle shi <154901060+lordshit@users.noreply.github.com>
2025-04-21 18:21:18 +0600

    Edit app.js

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 app.set ('view engine','ejs')
 app.get('/', (req, res) => {
-    console.log(res.url)
+    console.log(req.url)
     res.render('index');
 });
 app.get('/about.ejs', (req, res) => {
-    console.log(res.url)
+    console.log(req.url)
     res.render('about');
 });
 app.get('/contact-me.ejs', (req, res) => {
-    console.log(res.url)
+    console.log(req.url)
     res.render('contact-me');
 });
 app.get('/index.ejs', (req, res) => {
-    console.log(res.url)
+    console.log(req.url)
     res.render('index');
 });
 app.use((req, res) => {
     console.log(req.url)
     res.render('404');
 });
-app.listen(3000);
+app.listen(8080);
