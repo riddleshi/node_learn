efe15029d8f949c5151bbf2cd468bae480ac53ee
riddle shi <154901060+lordshit@users.noreply.github.com>
2025-04-21 18:01:53 +0600

    Add app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.set ('view engine','ejs')
+app.get('/', (req, res) => {
+    console.log(res.url)
+    res.render('index');
+});
+app.get('/about.ejs', (req, res) => {
+    console.log(res.url)
+    res.render('about');
+});
+app.get('/contact-me.ejs', (req, res) => {
+    console.log(res.url)
+    res.render('contact-me');
+});
+app.get('/index.ejs', (req, res) => {
+    console.log(res.url)
+    res.render('index');
+});
+app.use((req, res) => {
+    console.log(req.url)
+    res.render('404');
+});
+app.listen(3000);
